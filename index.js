f515ad87c4f43789c1439022315a1b3a72c76693
Sazid60 <shahnawazsazid60@gmail.com>
2024-06-01 09:49:28 +0600

    MongoDb Setup & User Data Stored

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjbmdks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+const usersCollection = client.db("Propex").collection("users")
+
+// saving user in database
+app.post('/user', async (req, res) => {
+    const user = req.body;
+
+
+    const query = { email: user.email }
+    const existingUser = await usersCollection.findOne(query);
+
+    if (existingUser) {
+      return res.send({ message: 'User Already Exist', insertedId: null })
+    }
+    const result = await usersCollection.insertOne(user);
+    res.send(result)
+  })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Propex is Running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Propex On on port ${port}` )
+})
